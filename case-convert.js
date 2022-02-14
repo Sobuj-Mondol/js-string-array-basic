83f022c9ffba0eb272e7e03ead68d152ba59e29e
Sobuj <sobujkumar424@gmail.com>
2022-02-14 09:13:18 +0600

    added case-convert

---
+++ case-convert.js
@@ -0,0 +1,8 @@
+let anthem = 'Amar Sonar Bangla Ami Tomay Valobashi';
+let search = 'valo';
+
+let userInput = 'SoBuj MonDol';
+let seveUserName = 'sobuj mondol';
+if(userInput.toLocaleLowerCase() == seveUserName.toLocaleLowerCase()){
+    console.log('user exists');
+}
